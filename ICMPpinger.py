a33b2f53239a106159ca221a9c68312e8d6d5a96
Winnie0v0 <wzong@smith.edu>
2022-09-29 00:18:33 -0400

    Fetch the ICMP header from the IP packet Make the header and append checksum Make a raw socket named mySocket Fetch the icmp type from the IP packet Resolve previous questions Add new todos

--- ICMPpinger.py
+++ ICMPpinger.py
@@ -64,12 +64,56 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         # Fill in start #
         #---------------#
 
-            # TODO: Fetch the ICMP header from the IP packet
+        # Task: Fetch the ICMP header from the IP packet
+
+        # Question: Where did we learned about the combinition of headers?
+        # Answer: It's in slack https://smith.enterprise.slack.com/files/U3ZF37THD/F043KU2LE7P/icmp_header_handout.pdf
+        header = recPacket[20:28]
+
+        # Here we can fetch the ICMP header from the IP packet
+        # But we don't need to return them all according to the assignment instruction.
+        type, code, checksum, id, sequence = struct.unpack(
+            "bbHHh", header)
+
+        # TODO: Does this means "receive the structure ICMP_ECHO_REPLY"
+        # if type == ICMP_ECHO_REQUEST:
+        # Do we need to check ID?
+        # if id ==ID:
+        # Why is type 0?
+        # Do we need to check anything here?
+        if type == 0:
+
+            # Question: is timeLeft equivalent to time to live (TTL)?
+            # Answer: No, TTL is not from icmp header but can be upacked from other packet
+                    # In the IPv4 header, TTL is the 9th octet of 20
+                    # https://en.wikipedia.org/wiki/Time_to_live
+                    # https://en.wikipedia.org/wiki/IPv4#Header
+                    # But it's a little hard to implement here because we don't have the format
+                    # https://docs.python.org/3/library/struct.html#format-characters
+            # TODO: Can we use the following code to get TTL?
+            # If so why don't we need to manually add them into the pack?
+            # Is it because they are not optional?
+            # ttl = struct.unpack("d", recPacket[9:10])
+            ttl = timeLeft-howLongInSelect
+            
+            # TODO: Is delay just round-trip time (RTT)?
+            # And for this assignment we only need to return delay?
+            bytes = struct.calcsize("d")
+            timeSent = struct.unpack("d", recPacket[28:28 + bytes])[0]
+            rrt = timeReceived - timeSent
+
+            # example from ping google in terminal
+            # 64 bytes from 142.251.40.196: icmp_seq=0 ttl=115 time=257.346 ms
+            # For python old string formatting
+            # https://docs.python.org/3/library/stdtypes.html#old-string-formatting
+            return " %d bytes from %s: icmp_seq=%d ttl=%.0f time=%.3f ms" %(bytes, destAddr, sequence, ttl*1000, rrt*1000)
+            # TODO: Should probably increase sequence somewhere
 
         #-------------#
         # Fill in end #
         #-------------#
 
+        # TODO: Is timeLeft the same as ttl (time to live)
         timeLeft = timeLeft - howLongInSelect 
         
         if timeLeft <= 0:
@@ -85,12 +129,14 @@ def sendOnePing(mySocket, destAddr, ID):
  
     # struct -- Interpret strings as packed binary data
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1) 
-    data = struct.pack("d", time.time())
+    data = struct.pack("d", time.time()) #format string
 
     # Calculate the checksum on the data and the dummy header. 
-    myChecksum = checksum(str(header + data))
+    # myChecksum = checksum(str(header + data))
+    myChecksum = checksum(''.join(map(chr, header+data)))
 
     # Get the right checksum, and put in the header 
+    # TODO: What does darwin means?
     if sys.platform == 'darwin':
         # Convert 16-bit integers from host to network byte order 
         myChecksum = htons(myChecksum) & 0xffff
@@ -100,6 +146,7 @@ def sendOnePing(mySocket, destAddr, ID):
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1) 
     packet = header + data
 
+    # TODO: What exactly is a socket?
     mySocket.sendto(packet, (destAddr, 1)) # AF_INET address must be tuple, not str 
     # Both LISTS and TUPLES consist of a number of objects
     # which can be referenced by their position number within the object.
@@ -107,6 +154,7 @@ def sendOnePing(mySocket, destAddr, ID):
 
 
 def doOnePing(destAddr, timeout): 
+    # TODO: What does this do?
     icmp = getprotobyname("icmp")
 
     # SOCK_RAW is a powerful socket type. For more details:	
@@ -139,4 +187,16 @@ def ping(host, timeout=1):
     return delay
 
 # Runs program
-ping("google.com")
+# TODO
+if __name__ == '__main__':
+    print("www.google.com in North America")
+    ping("www.google.com")
+    print("")
+    print("www.yahoo.co.in in Asia")
+    ping("www.yahoo.co.in")
+    print("")
+    print("www.ox.ac.uk in Europe")
+    ping("www.ox.ac.uk")
+    print("")
+    print("www.ru.ac.za in Africa")
+    ping("www.ru.ac.za")
--- ICMPtraceroute.py
+++ ICMPtraceroute.py
@@ -27,8 +27,42 @@ def build_packet():
     # Fill in start #
     #---------------#
 
-        # TODO: Make the header in a similar way to the ping exercise.
-        # Append checksum to the header.
+    # Task: Make the header in a similar way to the ping exercise.
+
+    # Header is type (8), code (8), checksum (16), id (16), sequence (16)
+    myChecksum = 0
+    myID = os.getpid() & 0xFFFF
+
+    # Question: What is "bbHHh"
+    # Answer: It's format charecters
+    # https://docs.python.org/3/library/struct.html#format-characters
+
+    # Question: We don't need ID in this header?
+    # Answer: We do need because otherwise pack method is not happy
+    
+    # Make a dummy header with a 0 checksum
+    # struct -- Interpret strings as packed binary data
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, myID, 1) 
+    data = struct.pack("d", time.time())
+
+    # Task: Append checksum to the header.
+
+    # Calculate the checksum on the data and the dummy header. 
+    # myChecksum = checksum(str(header + data))
+    myChecksum = checksum(''.join(map(chr, header+data)))
+
+    # Get the right checksum, and put in the header 
+
+    # Question: What is darwin?
+    # Answer: It's to check what's your operating system
+    # Interestingly Mac OS x is darwin
+    if sys.platform == 'darwin':
+        # Convert 16-bit integers from host to network byte order 
+        myChecksum = htons(myChecksum) & 0xffff
+    else:
+        myChecksum = htons(myChecksum)
+
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, myID, 1) 
         
     #-------------#
     # Fill in end #
@@ -48,7 +82,17 @@ def get_route(hostname):
             # Fill in start #
             #---------------#
 
-                # TODO: Make a raw socket named mySocket
+            # Task: Make a raw socket named mySocket
+
+            # Question: What's icmp here?
+            # Answer: icmp is a protocal which provides a common ground for computers to communicate
+            # TODO: Look into protocals
+            icmp = getprotobyname("icmp") 
+
+            # SOCK_RAW is a powerful socket type. For more details:	
+            # http://sock-raw.org/papers/sock_raw
+
+            mySocket = socket(AF_INET, SOCK_RAW, icmp)
 
             #-------------#
             # Fill in end #
@@ -83,12 +127,15 @@ def get_route(hostname):
                 # Fill in start #
                 #---------------#
 
-                    #TODO: Fetch the icmp type from the IP packet
+                #Task: Fetch the icmp type from the IP packet
+                header = recvPacket[20:28]
+                types, code, checksum, packetID, sequence= struct.unpack("bbHHh", header)
 
                 #-------------#
                 # Fill in end #
                 #-------------#
                 
+                # TODO: Why are there different types?
                 if types == 11:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 +bytes])[0]
@@ -113,4 +160,18 @@ def get_route(hostname):
             finally:
                 mySocket.close()
 
-get_route("google.com")
+# TODO
+if __name__ == '__main__':
+    print("www.google.com in North America")
+    get_route("www.google.com")
+    print("")
+    print("www.yahoo.co.in in Asia")
+    get_route("www.yahoo.co.in")
+    print("")
+    print("www.ox.ac.uk in Europe")
+    get_route("www.ox.ac.uk")
+    print("")
+    print("www.ru.ac.za in Africa")
+    get_route("www.ru.ac.za")
+
+# TODO: Reflection
